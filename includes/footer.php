8d725526e873b4e747fdbe04008884ff3062adc2
shyjijohn <shyjijohn90@gmail.com>
2026-06-04 15:49:31 +0100

    changes using php

---
+++ includes/footer.php
@@ -0,0 +1,89 @@
+<!-- FOOTER -->
+
+    <footer class="footer">
+      <div class="container">
+        <div class="footer__main">
+          <div class="footer__brand">
+            <img src="assets/logo-white.png" alt="Netmatters" class="footer__logo">
+            <p class="footer__title">FOLLOW US ON:</p>
+            <div class="footer__socials">
+              <a href="#" class="social-box social-icon facebook"><img src="assets/facebook-white.png"
+                  alt="Facebook"></a>
+              <a href="#" class="social-box social-icon linkedin"><img src="assets/linkedin-white.png"
+                  alt="LinkedIn"></a>
+              <a href="#" class="social-box social-icon twitter"><img src="assets/twitter-white.png" alt="Twitter"></a>
+              <a href="#" class="social-box social-icon instagram"><img src="assets/instagram-white.png"
+                  alt="Instagram"></a>
+            </div>
+          </div>
+
+          <div class="footer__row">
+            <div class="footer__menu">
+              <h3>ABOUT NETMATTERS</h3>
+              <ul>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Our Careers</a></li>
+                <li><a href="#">Our Team</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Cookie Policy</a></li>
+                <li><a href="#">Data Retention</a></li>
+                <li><a href="#">CCTV Policy</a></li>
+                <li><a href="#">Environmental Policy</a></li>
+                <li><a href="#">Sexual Harassment Policy</a></li>
+                <li><a href="#">Modern Slavery Statement</a></li>
+                <li><a href="#">Terms & Conditions</a></li>
+              </ul>
+            </div>
+
+            <div class="footer__menu">
+              <h3>OUR SERVICES</h3>
+              <ul>
+                <li><a href="#">Bespoke Software</a></li>
+                <li><a href="#">IT Support</a></li>
+                <li><a href="#">Digital Marketing</a></li>
+                <li><a href="#">Telecoms Services</a></li>
+                <li><a href="#">Web Design</a></li>
+                <li><a href="#">Cyber Security</a></li>
+                <li><a href="#">Developer Training</a></li>
+              </ul>
+            </div>
+
+            <div class="footer__menu">
+              <h3>OUR INDUSTRIES</h3>
+              <ul>
+                <li><a href="#">Financial Services</a></li>
+                <li><a href="#">Construction</a></li>
+                <li><a href="#">Retail & E-Commerce</a></li>
+                <li><a href="#">Non-Profits</a></li>
+                <li><a href="#">SME's</a></li>
+                <li><a href="#">Healthcare</a></li>
+                <li><a href="#">Education & Training</a></li>
+                <li><a href="#">Travel & Leisure</a></li>
+                <li><a href="#">Manufacturing & Offshore</a></li>
+              </ul>
+            </div>
+
+            <div class="footer__menu">
+              <h3>LOCATIONS</h3>
+              <ul>
+                <li><a href="#">Cambridge Office</a></li>
+                <li><a href="#">Wymondham Office</a></li>
+                <li><a href="#">Great Yarmouth Office</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="footer__bottom">
+        <div class="container">
+          <p>&copy; Copyright Netmatters 2025.</p>
+          <p class="desktop-only"> &nbsp;- &nbsp;</p>
+          <p>All rights reserved.</p>
+          <p><a href="#" style="text-decoration: none;">Sitemap</a></p>
+        </div>
+      </div>
+    </footer>
+
+  </div><!-- end #pagePushWrapper -->
