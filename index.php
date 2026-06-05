d96caeefdf4465b51da1bffcea9af4b491b79202
shyjijohn <shyjijohn90@gmail.com>
2026-06-05 20:37:12 +0100

    changes

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
+
 <?php include 'includes/header.php'; ?>
 <?php include 'includes/menu.php'; ?>
 
